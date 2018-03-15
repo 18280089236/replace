2b29ceaeebbd065ca40e4e4c61d69ab7c6c84989
hez <hez@192.168.0.3>
2018-03-15 18:18:44 +0800

    初始化上传

---
+++ src/main/java/com/fyx/utils/JsonpFileUtils.java
@@ -0,0 +1,70 @@
+package com.fyx.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by hezhao on 2018/3/8.
+ */
+public class JsonpFileUtils {
+
+    public static Document parserJsoup(File file) throws Exception {
+        StringBuffer buffer = new StringBuffer();
+        BufferedReader bf = new BufferedReader(new FileReader(file));
+        String s = null;
+        while ((s = bf.readLine()) != null) {//使用readLine方法，一次读一行
+            buffer.append(s.trim()+"\r\n");
+        }
+
+        String xml = buffer.toString();
+        Document doc= Jsoup.parse(xml);
+        bf.close();
+        return doc;
+    }
+
+    public static int replace(Map<String, String> properties, Elements allElements) {
+        int size = allElements.size();
+        AtomicInteger num = new AtomicInteger(0);
+        for (int i = 0; i < size; i++) {
+
+            Element element = allElements.get(i);
+            if (element.ownText() == null || element.ownText().trim().equals("")) {
+                continue;
+            }
+            //查找是否包含map键
+            String textContent = element.ownText().trim().replace("\t","").replace("\n","");
+
+            properties.forEach((k, v) -> {
+                if (textContent.length() < 20 && textContent.equals(k)) {
+                    if (!element.hasClass("i18n")) {
+                        element.addClass("i18n");
+                    }
+                    element.attr("fyx", v);
+                    num.addAndGet(1);
+                }
+            });
+        }
+        return num.get();
+    }
+
+    public static void write(Document document, File file) {
+        String html = document.html();
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            PrintStream ps = new PrintStream(fileOutputStream);
+            ps.println(html);// 往文件里写入字符串
+            fileOutputStream.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
